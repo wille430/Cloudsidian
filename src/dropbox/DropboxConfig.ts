61c565786b73dd3f8680ded68042c02a236c6e73
William Wigemo <william.wigemo@outlook.com>
2023-02-16 15:50:57 +0100

    fix: correct oauth redirect uri

--- src/dropbox/DropboxConfig.ts
+++ src/dropbox/DropboxConfig.ts
@@ -1,6 +1,7 @@
 export class DropboxConfig {
     public static CLIENT_ID = "8h765h9g88sgqhp"
     public static REDIRECT_PATH = "/auth/dropbox-signin"
-    public static REDIRECT_URI = `http://localhost:3000${DropboxConfig.REDIRECT_PATH}`
+    // process.env.URL is set in Netlify build environment
+    public static REDIRECT_URI = `${process.env.URL ?? "http://localhost:3000"}${DropboxConfig.REDIRECT_PATH}`
     public static CODE_KEY = "code"
 }
